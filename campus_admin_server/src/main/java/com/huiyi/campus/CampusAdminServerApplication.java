2aed816657979ec1cea58d56e019e3fe1a10c180
李玉凯 <2919529566@qq.com>
2021-04-03 21:39:37 +0800

    1、增加微服务依赖，增加健康检查与健康模板通知，更改日志文件存储 2、nacos注册依赖与yml配置

---
+++ campus_admin_server/src/main/java/com/huiyi/campus/CampusAdminServerApplication.java
@@ -0,0 +1,21 @@
+package com.huiyi.campus;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author: liyukai
+ * @Email: 2919529566@qq.com
+ * @Description: 监控服务器启动类 -健康监控后台访问地址http://192.168.123.13:8769/
+ * @date: 2021-04-03 18:14
+ * @Version V1.0
+ */
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableAdminServer
+public class CampusAdminServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CampusAdminServerApplication.class, args);
+    }
+}
--- campus_startup/src/main/java/com/huiyi/campus/CampusStartApplication.java
+++ campus_startup/src/main/java/com/huiyi/campus/CampusStartApplication.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @Version V1.0
  */
 @Slf4j
+@EnableDiscoveryClient
 @ServletComponentScan
 @SpringBootApplication(scanBasePackages = "com.huiyi.campus")
 @MapperScan("com.huiyi.campus.dao.mapper")
@@ -31,24 +33,24 @@ public class CampusStartApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(CampusStartApplication.class, args);
-//        log.info(
-//                "\n\n\n\n\n                                                                                                                                   dddddddd\n"
-//                        + "                          tttt                                                         tttt                                          d::::::d\n"
-//                        + "                       ttt:::t                                                      ttt:::t                                          d::::::d\n"
-//                        + "                       t:::::t                                                      t:::::t                                          d::::::d\n"
-//                        + "                       t:::::t                                                      t:::::t                                          d:::::d \n"
-//                        + "    ssssssssss   ttttttt:::::ttttttt      aaaaaaaaaaaaa   rrrrr   rrrrrrrrr   ttttttt:::::ttttttt        eeeeeeeeeeee        ddddddddd:::::d \n"
-//                        + "  ss::::::::::s  t:::::::::::::::::t      a::::::::::::a  r::::rrr:::::::::r  t:::::::::::::::::t      ee::::::::::::ee    dd::::::::::::::d \n"
-//                        + "ss:::::::::::::s t:::::::::::::::::t      aaaaaaaaa:::::a r:::::::::::::::::r t:::::::::::::::::t     e::::::eeeee:::::ee d::::::::::::::::d \n"
-//                        + "s::::::ssss:::::stttttt:::::::tttttt               a::::a rr::::::rrrrr::::::rtttttt:::::::tttttt    e::::::e     e:::::ed:::::::ddddd:::::d \n"
-//                        + " s:::::s  ssssss       t:::::t              aaaaaaa:::::a  r:::::r     r:::::r      t:::::t          e:::::::eeeee::::::ed::::::d    d:::::d \n"
-//                        + "   s::::::s            t:::::t            aa::::::::::::a  r:::::r     rrrrrrr      t:::::t          e:::::::::::::::::e d:::::d     d:::::d \n"
-//                        + "      s::::::s         t:::::t           a::::aaaa::::::a  r:::::r                  t:::::t          e::::::eeeeeeeeeee  d:::::d     d:::::d \n"
-//                        + "ssssss   s:::::s       t:::::t    tttttta::::a    a:::::a  r:::::r                  t:::::t    tttttte:::::::e           d:::::d     d:::::d \n"
-//                        + "s:::::ssss::::::s      t::::::tttt:::::ta::::a    a:::::a  r:::::r                  t::::::tttt:::::te::::::::e          d::::::ddddd::::::dd\n"
-//                        + "s::::::::::::::s       tt::::::::::::::ta:::::aaaa::::::a  r:::::r                  tt::::::::::::::t e::::::::eeeeeeee   d:::::::::::::::::d\n"
-//                        + " s:::::::::::ss          tt:::::::::::tt a::::::::::aa:::a r:::::r                    tt:::::::::::tt  ee:::::::::::::e    d:::::::::ddd::::d\n"
-//                        + "  sssssssssss              ttttttttttt    aaaaaaaaaa  aaaa rrrrrrr                      ttttttttttt      eeeeeeeeeeeeee     ddddddddd   ddddd\n\n\n\n\n");
+        log.info(
+                "\n\n\n\n\n                                                                                                                                   dddddddd\n"
+                        + "                          tttt                                                         tttt                                          d::::::d\n"
+                        + "                       ttt:::t                                                      ttt:::t                                          d::::::d\n"
+                        + "                       t:::::t                                                      t:::::t                                          d::::::d\n"
+                        + "                       t:::::t                                                      t:::::t                                          d:::::d \n"
+                        + "    ssssssssss   ttttttt:::::ttttttt      aaaaaaaaaaaaa   rrrrr   rrrrrrrrr   ttttttt:::::ttttttt        eeeeeeeeeeee        ddddddddd:::::d \n"
+                        + "  ss::::::::::s  t:::::::::::::::::t      a::::::::::::a  r::::rrr:::::::::r  t:::::::::::::::::t      ee::::::::::::ee    dd::::::::::::::d \n"
+                        + "ss:::::::::::::s t:::::::::::::::::t      aaaaaaaaa:::::a r:::::::::::::::::r t:::::::::::::::::t     e::::::eeeee:::::ee d::::::::::::::::d \n"
+                        + "s::::::ssss:::::stttttt:::::::tttttt               a::::a rr::::::rrrrr::::::rtttttt:::::::tttttt    e::::::e     e:::::ed:::::::ddddd:::::d \n"
+                        + " s:::::s  ssssss       t:::::t              aaaaaaa:::::a  r:::::r     r:::::r      t:::::t          e:::::::eeeee::::::ed::::::d    d:::::d \n"
+                        + "   s::::::s            t:::::t            aa::::::::::::a  r:::::r     rrrrrrr      t:::::t          e:::::::::::::::::e d:::::d     d:::::d \n"
+                        + "      s::::::s         t:::::t           a::::aaaa::::::a  r:::::r                  t:::::t          e::::::eeeeeeeeeee  d:::::d     d:::::d \n"
+                        + "ssssss   s:::::s       t:::::t    tttttta::::a    a:::::a  r:::::r                  t:::::t    tttttte:::::::e           d:::::d     d:::::d \n"
+                        + "s:::::ssss::::::s      t::::::tttt:::::ta::::a    a:::::a  r:::::r                  t::::::tttt:::::te::::::::e          d::::::ddddd::::::dd\n"
+                        + "s::::::::::::::s       tt::::::::::::::ta:::::aaaa::::::a  r:::::r                  tt::::::::::::::t e::::::::eeeeeeee   d:::::::::::::::::d\n"
+                        + " s:::::::::::ss          tt:::::::::::tt a::::::::::aa:::a r:::::r                    tt:::::::::::tt  ee:::::::::::::e    d:::::::::ddd::::d\n"
+                        + "  sssssssssss              ttttttttttt    aaaaaaaaaa  aaaa rrrrrrr                      ttttttttttt      eeeeeeeeeeeeee     ddddddddd   ddddd\n\n\n\n\n");
 
     }
 
