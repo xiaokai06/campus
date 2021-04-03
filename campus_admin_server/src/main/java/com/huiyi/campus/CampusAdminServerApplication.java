d81ac09c5c83040942b3d9b7b6badf3a95bb4028
李玉凯 <2919529566@qq.com>
2021-04-04 00:41:34 +0800

    1、修改日志存储，2、修改数据库sql打印，3、修改新服务无法注册监控

--- campus_admin_server/src/main/java/com/huiyi/campus/CampusAdminServerApplication.java
+++ campus_admin_server/src/main/java/com/huiyi/campus/CampusAdminServerApplication.java
@@ -4,6 +4,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author: liyukai
@@ -14,6 +15,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  */
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableAdminServer
+@EnableDiscoveryClient
 public class CampusAdminServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CampusAdminServerApplication.class, args);
