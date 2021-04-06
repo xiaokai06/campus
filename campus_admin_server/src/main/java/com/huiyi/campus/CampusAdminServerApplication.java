410f9c3923aa6bf403182fa42a13c32e80359c06
李玉凯 <2919529566@qq.com>
2021-04-06 16:49:09 +0800

    1、暂时注释nacos注册中心

--- campus_admin_server/src/main/java/com/huiyi/campus/CampusAdminServerApplication.java
+++ campus_admin_server/src/main/java/com/huiyi/campus/CampusAdminServerApplication.java
@@ -1,23 +1,23 @@
-package com.huiyi.campus;
-
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-/**
- * @author: liyukai
- * @Email: 2919529566@qq.com
- * @Description: 监控服务器启动类 -健康监控后台访问地址http://192.168.123.13:8769/
- * @date: 2021-04-03 18:14
- * @Version V1.0
- */
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@EnableAdminServer
-@EnableDiscoveryClient
-public class CampusAdminServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CampusAdminServerApplication.class, args);
-    }
-}
+//package com.huiyi.campus;
+//
+//import de.codecentric.boot.admin.server.config.EnableAdminServer;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//
+///**
+// * @author: liyukai
+// * @Email: 2919529566@qq.com
+// * @Description: 监控服务器启动类 -健康监控后台访问地址http://192.168.123.13:8769/
+// * @date: 2021-04-03 18:14
+// * @Version V1.0
+// */
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+//@EnableAdminServer
+//@EnableDiscoveryClient
+//public class CampusAdminServerApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(CampusAdminServerApplication.class, args);
+//    }
+//}
