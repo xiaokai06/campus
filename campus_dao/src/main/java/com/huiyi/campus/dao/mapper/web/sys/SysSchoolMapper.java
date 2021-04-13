1f990a7f86e5ca3a542f8a58edef614c04a19842
杨郅刚 <yangzhigang.lucky@qq.com>
2021-04-13 16:35:55 +0800

    优化代码

--- campus_dao/src/main/java/com/huiyi/campus/dao/mapper/web/sys/SysSchoolMapper.java
+++ campus_dao/src/main/java/com/huiyi/campus/dao/mapper/web/sys/SysSchoolMapper.java
@@ -8,29 +8,44 @@ public interface SysSchoolMapper {
 
     /**
      * 查询学校
-     * @param sysSchoolEntity
-     * @return
+     * @param sysSchoolEntity 参数
+     * @return 返回值
      */
     List<SysSchoolEntity> selectAllSchool(SysSchoolEntity sysSchoolEntity);
 
     /**
      * 新增学校
-     * @param sysSchoolEntity
-     * @return
+     * @param sysSchoolEntity 参数
+     * @return 返回值
      */
     int insertSchoolInfo(SysSchoolEntity sysSchoolEntity);
 
     /**
      * 修改学校
-     * @param sysSchoolEntity
-     * @return
+     * @param sysSchoolEntity 参数
+     * @return 返回值
      */
     int updateSchoolInfo(SysSchoolEntity sysSchoolEntity);
 
     /**
      * 删除学校
-     * @param id
-     * @return
+     * @param id 参数
+     * @return 返回值
      */
     int deleteSchoolInfo(Integer id);
+
+
+    /**
+     * 根据用户ID查询当前所归属学校
+     * @param id 参数
+     * @return 返回值
+     */
+    List<SysSchoolEntity> selectSchoolByUserId(Integer id);
+
+    /**
+     * 根据教育局ID与用户ID查询学校
+     * @param id 参数
+     * @return 返回值
+     */
+    List<SysSchoolEntity> selectSchoolByUserIdAndOrganId(Integer id);
 }
