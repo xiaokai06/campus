39dd33e2ade9f7d3678aa9d6246c2a4626d6c967
李玉凯 <2919529566@qq.com>
2021-04-16 18:23:46 +0800

    增加健康评估，家庭信息

---
+++ campus_web/src/main/java/com/huiyi/campus/web/health/controller/HealthAssessmentController.java
@@ -0,0 +1,16 @@
+package com.huiyi.campus.web.health.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: liyukai
+ * @Email: 2919529566@qq.com
+ * @Description: 健康体检报告管理-健康评估
+ * @date: 2021-04-16 18:20
+ * @Version V1.0
+ */
+@RestController
+@RequestMapping("/assessment")
+public class HealthAssessmentController {
+}
