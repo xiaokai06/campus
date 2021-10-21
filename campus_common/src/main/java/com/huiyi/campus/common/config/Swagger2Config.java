c58483e3ce38435de6c9fc3a749b41843c9eba96
yukaili <2919529566@qq.com>
2021-10-21 22:44:22 +0800

    提交OCR

--- campus_common/src/main/java/com/huiyi/campus/common/config/Swagger2Config.java
+++ campus_common/src/main/java/com/huiyi/campus/common/config/Swagger2Config.java
@@ -1,61 +1,73 @@
 package com.huiyi.campus.common.config;
 
+import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import static com.google.common.collect.Lists.newArrayList;
+
 /**
- * @author yzg
- * @date 2020/9/22 0022
- * @description: swagger2的配置文件，在项目的启动类的同级文件建立
+ * swagger配置
  */
+@Slf4j
 @Configuration
 @EnableSwagger2
-public class Swagger2Config {
-
+@EnableSwaggerBootstrapUI
+public class Swagger2Config implements WebMvcConfigurer {
     /**
-     * swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
+     * 显示swagger-ui.html文档展示页，还必须注入swagger资源：
+     *
+     * @param registry
      */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
     @Bean
     public Docket createRestApi() {
-        ParameterBuilder tokenPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<>();
-        tokenPar.name("token").description("token").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
-        pars.add(tokenPar.build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                // 为当前包路径
-                .apis(RequestHandlerSelectors.basePackage("com.huiyi.campus.web"))
+                //加了ApiOperation注解的类，才生成接口文档
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                //包下的类，才生成接口文档
+                //.apis(RequestHandlerSelectors.basePackage("com.gennlife.controller"))
                 .paths(PathSelectors.any())
                 .build()
-                .globalOperationParameters(pars);
+//                .directModelSubstitute(java.util.Date.class, String.class)
+                .securitySchemes(security());
     }
 
-    /**
-     * 构建 api文档的详细信息函数,注意这里的注解引用的是哪个
-     */
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                // 页面标题
-                .title("Spring Boot 测试使用 Swagger2 构建RESTFUL API")
-                // 版本号
-                .version("1.0")
-                // 描述
-                .description("API 描述")
+                .title("校园健康管理平台")
+                .description("接口文档")
+                .termsOfServiceUrl("")
+                .version("3.0.0")
                 .build();
+    }
+
+    private List<ApiKey> security() {
+        return newArrayList(
+                new ApiKey("Authorization", "Authorization", "header")
+        );
     }
 
 }
---
+++ campus_web/src/main/java/com/huiyi/campus/demo/HuaweiOcrTest.java
@@ -0,0 +1,11 @@
+package com.huiyi.campus.demo;
+
+/**
+ * @author: liyukai
+ * @Email: 2919529566@qq.com
+ * @Description:
+ * @date: 2021/5/20 10:46 上午
+ * @Version V1.0
+ */
+public class HuaweiOcrTest {
+}
